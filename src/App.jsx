22a58a44b5e91ed00fba8b5f15b7e08fa5b9e829
Super-Proxy <100104076+Super-Proxy@users.noreply.github.com>
2026-05-26 16:08:22 +0800

    adding Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Homepage } from './Pages/Homepage';
+import { Routes, Route } from 'react-router'  
 import './App.css'
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="checkout" element={<h1>Checkout Page</h1>} />
+    </Routes>
   )
 }
 
